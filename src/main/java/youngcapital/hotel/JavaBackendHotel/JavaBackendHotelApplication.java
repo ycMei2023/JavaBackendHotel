0e17140b97038588a0b9aa6c6d0a1c5834a75697
prinsjurgen@gmail.com <prinsjurgen@gmail.com>
2023-05-22 10:27:38 +0200

    default setup for spring

---
+++ src/main/java/youngcapital/hotel/JavaBackendHotel/JavaBackendHotelApplication.java
@@ -0,0 +1,13 @@
+package youngcapital.hotel.JavaBackendHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendHotelApplication.class, args);
+	}
+
+}
