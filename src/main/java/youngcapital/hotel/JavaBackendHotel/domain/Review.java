e5d9ed24ca342ed3fba8aeea2f6f8cc24f41a36a
prinsjurgen@gmail.com <prinsjurgen@gmail.com>
2023-05-22 16:11:56 +0200

    classic table for review

---
+++ src/main/java/youngcapital/hotel/JavaBackendHotel/domain/Review.java
@@ -0,0 +1,53 @@
+package youngcapital.hotel.JavaBackendHotel.domain;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private  int rating;
+    private String name, reviewText;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public Review() {
+    }
+}
