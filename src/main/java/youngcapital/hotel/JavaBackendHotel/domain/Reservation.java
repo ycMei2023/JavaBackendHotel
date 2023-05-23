f66b75db7c34bf53f25adff9db5888166a392e37
Ian <ianlippelt@gmail.com>
2023-05-23 11:47:53 +0200

    add missing reservation properties

--- src/main/java/youngcapital/hotel/JavaBackendHotel/domain/Reservation.java
+++ src/main/java/youngcapital/hotel/JavaBackendHotel/domain/Reservation.java
@@ -11,34 +11,78 @@ public class Reservation {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	private int roomNumber;
-	private boolean breakfast;
 	private int amountPeople;
-	
+	private boolean breakfast;
+	private boolean business;
+	private boolean paymentStatus;
+	private float price;
+	private float discount;
+	//private Date beginDate;
+	//private Date endDate;
+	//private Date approvalDate;
+
+	public boolean isBusiness() {
+		return business;
+	}
+
+	public void setBusiness(boolean business) {
+		this.business = business;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public float getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(float discount) {
+		this.discount = discount;
+	}
+
+	public boolean isPaymentStatus() {
+		return paymentStatus;
+	}
+
+	public void setPaymentStatus(boolean paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public int getRoomNumber() {
 		return roomNumber;
 	}
+
 	public void setRoomNumber(int roomNumber) {
 		this.roomNumber = roomNumber;
 	}
+
 	public boolean isBreakfast() {
 		return breakfast;
 	}
+
 	public void setBreakfast(boolean breakfast) {
 		this.breakfast = breakfast;
 	}
+
 	public int getAmountPeople() {
 		return amountPeople;
 	}
+
 	public void setAmountPeople(int amountPeople) {
 		this.amountPeople = amountPeople;
 	}
-
-	
 
 }
