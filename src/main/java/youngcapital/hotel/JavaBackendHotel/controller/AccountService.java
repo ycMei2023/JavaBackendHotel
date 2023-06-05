845d8009780813404138de7489a61b3f946a73c5
jprins1234 <prinsjurgen@gmail.com>
2023-06-05 13:49:27 +0200

    you can create a account on register.html and it will be put in the database with a hashed password

---
+++ src/main/java/youngcapital/hotel/JavaBackendHotel/controller/AccountService.java
@@ -0,0 +1,19 @@
+package youngcapital.hotel.JavaBackendHotel.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import youngcapital.hotel.JavaBackendHotel.domain.Account;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    public Iterable<Account> getAllAccounts(){ return accountRepository.findAll();}
+
+    public void saveAccount(Account account){
+        account.encrypt();
+        accountRepository.save(account);
+    }
+}
