e1cdaad4f39ba694b89f1a3aa975fe0996244a22
JeroenvdenB <115401627+JeroenvdenB@users.noreply.github.com>
2023-06-07 16:27:06 +0200

    working on retrieving reserved rooms

--- src/main/java/youngcapital/hotel/JavaBackendHotel/controller/RoomRepository.java
+++ src/main/java/youngcapital/hotel/JavaBackendHotel/controller/RoomRepository.java
@@ -1,11 +1,19 @@
 package youngcapital.hotel.JavaBackendHotel.controller;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
+import youngcapital.hotel.JavaBackendHotel.domain.Reservation;
 import youngcapital.hotel.JavaBackendHotel.domain.Room;
 
 @Component
 public interface RoomRepository extends CrudRepository<Room, Long> {
+	// SELECT * FROM `reservation_rooms` WHERE `reservation_id` = ?1
+	// doesn't work, Room expects a column 'id' and it complains its not found
+	@Query(value = "SELECT * FROM Room", nativeQuery = true)
+	List<Room> getReservedRooms();
 	
 }
--- src/main/java/youngcapital/hotel/JavaBackendHotel/controller/RoomService.java
+++ src/main/java/youngcapital/hotel/JavaBackendHotel/controller/RoomService.java
@@ -1,31 +1,52 @@
 package youngcapital.hotel.JavaBackendHotel.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import youngcapital.hotel.JavaBackendHotel.domain.Reservation;
 import youngcapital.hotel.JavaBackendHotel.domain.Room;
 
 @Service
 public class RoomService {
 	@Autowired
-	RoomRepository rr;
+	RoomRepository roomRepository;
+	
+	@Autowired
+	ReservationRepository reservationRepository;
 	
 	public Optional<Room> findById(long id) {
-		return rr.findById(id);
+		return roomRepository.findById(id);
 	}
 	
 	public Iterable<Room> showAllRooms() {
-		return rr.findAll();
+		return roomRepository.findAll();
 	}
 	
 	public void saveRoom(Room room) {
-		rr.save(room);
+		roomRepository.save(room);
 	}
 
 	public void deleteRoom(long id) {
-		rr.deleteById(id);
+		roomRepository.deleteById(id);
 		
+	}
+
+	public Iterable<Room> getReservedRooms() {
+		List<Reservation> allReservations = reservationRepository.reservations();
+		
+		// Code to filter for date on reservations goes here
+		// Return all id's of reservations that overlap with the entered date range
+		// Rooms connected to those id's are occupied on the entered date range
+		// For now all reservation id's are used, to get the rooms associated with them
+		
+		long[] matchingReservationIds = {16, 17, 18, 19};
+		
+		// TO-DO: use gathered reservations (List<Reservation> or long[] with id's)
+		//			to get all the associated rooms. Return List<Room>.
+		
+		return roomRepository.getReservedRooms();
 	}
 }
